a1eaf07eb81de1659505252e203dadb16d735300
Michael Ebnicher <m.ebnicher@tum.de>
2021-09-12 15:55:00 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jmd',
+    version='0.0',
+    description='A useful module',
+    author='Michael Ebnicher',
+    author_email='foomail@foo.com',
+    packages=['jmd'],  # same as name
+    # external packages as dependencies
+    install_requires=['python-frontmatter'],
+)
