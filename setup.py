77c179cf3210c23696aceebfe509716264857063
Michael Ebnicher <m.ebnicher@tum.de>
2021-09-12 16:10:40 +0200

    revert

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-import setuptools
-setuptools.setup()
-
-# from setuptools import setup
-# setup(
-#     name='jmd',
-#     version='0.0',
-#     description='A useful module',
-#     author='Michael Ebnicher',
-#     author_email='foomail@foo.com',
-#     packages=['jmd'],  # same as name
-#     # external packages as dependencies
-#     install_requires=['python-frontmatter==1.0.0',
-#                       'os',
-#                       're',
-#                       'argparse',
-#                       'numpy'],
-# )
+from setuptools import setup
+setup(
+    name='jmd',
+    version='0.0',
+    description='A useful module',
+    author='Michael Ebnicher',
+    author_email='foomail@foo.com',
+    packages=['jmd'],  # same as name
+    # external packages as dependencies
+    install_requires=['python-frontmatter==1.0.0',
+                      'os',
+                      're',
+                      'argparse',
+                      'numpy'],
+)
