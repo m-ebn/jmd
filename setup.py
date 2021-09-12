b1f30e5e6b8ef7eaf550357b7db714966a260a16
Michael Ebnicher <m.ebnicher@tum.de>
2021-09-12 16:01:17 +0200

    frontmatter v 1.0.0

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ setup(
     author_email='foomail@foo.com',
     packages=['jmd'],  # same as name
     # external packages as dependencies
-    install_requires=['python-frontmatter', 'os', 're', 'argparse', 'numpy'],
+    install_requires=['python-frontmatter==1.0.0',
+                      'os',
+                      're',
+                      'argparse',
+                      'numpy'],
 )
